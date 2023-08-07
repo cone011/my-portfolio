99ff19e6ad2c9a3f418e65b3b4e62a04f0391de8
cone011 <jjcaimen@gmail.com>
2023-08-07 08:06:55 -0300

    checkpoint

--- src/Components/UI/Button.js
+++
@@ -1,8 +0,0 @@
-import { Buttons } from "../../Styles/Button";
-
-const Button = (props) => {
-  const { text } = props;
-  return <Buttons>{text}</Buttons>;
-};
-
-export default Button;
--- src/Components/UI/Confetti/ConfettiCompoment.js
+++
@@ -1,9 +0,0 @@
-import Confetti from "react-confetti";
-import { useWindowSizes } from "react-use-window-sizes";
-
-const ConfettiCompoment = () => {
-  const { width, height } = useWindowSizes();
-  return <Confetti numberOfPieces={300} width={width} height={height} />;
-};
-
-export default ConfettiCompoment;
